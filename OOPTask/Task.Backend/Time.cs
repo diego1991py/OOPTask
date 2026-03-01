108886c89c8eaec97973d58f6adf8c84bb6bd360
Diego Valencia <91valenciadiego@gmail.com>
2026-02-28 21:36:24 -0500

    Initial commit

---
+++ OOPTask/Task.Backend/Time.cs
@@ -0,0 +1,116 @@
+﻿using System.ComponentModel;
+
+namespace Task.Backend
+{
+    public class Time
+    {
+        private int _hour;
+        private int _minute;
+        private int _second;
+        private int _millisecond;
+
+        public Time()
+        {
+        }
+
+        public Time(int hour)
+        {
+            Hour = hour;
+        }
+
+        public Time(int hour, int minute)
+        {
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public Time(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public Time(int hour, int minute, int second, int millisecond)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            Millisecond = millisecond;
+        }
+
+        public int Hour
+        {
+          get => _hour;
+          set => _hour = ValidHour(value);
+        }
+        public int Minute
+        { 
+          get => _minute;
+          set => _minute = ValidMinute(value);
+        }
+        public int Second
+        { 
+          get => _second;
+          set => _second = ValidSecond(value);
+        }
+        public int Millisecond
+        { 
+          get => _millisecond;
+          set => _millisecond = ValidMillisecond(value);
+        }
+
+        public override string ToString()
+        {
+            if (Hour >= 12)
+            {
+                Hour -= 12;
+                return $"{Hour:00}:{Minute:00}:{Second:00}.{Millisecond:00} PM";
+            }            
+            return $"{Hour:00}:{Minute:00}:{Second:00}.{Millisecond:00} AM";
+        }
+
+
+        private int ValidHour(int hour)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hour), "The hour value must be between 0 and 23");
+            }
+            return hour;
+        }
+
+        private int ValidMinute(int minute)
+        {
+            if (minute < 0 || minute > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minute), "The hour value must be between 0 and 59");
+            }
+            return minute;
+        }
+
+        private int ValidSecond(int second)
+        {
+            if (second < 0 || second > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(second), "The hour value must be between 0 and 59");
+            }   
+            return second;
+        }
+
+        private int ValidMillisecond(int millisecond)
+        {
+            if (millisecond < 0 || millisecond > 999)
+            {
+                throw new ArgumentOutOfRangeException(nameof(millisecond), "The hour value must be between 0 and 999");
+            }
+            return millisecond;
+        }
+
+        //public int ToMillisecond()
+        //{
+
+        //}
+
+    }
+} 
---
+++ OOPTask/Task.Frontend/Program.cs
@@ -0,0 +1,14 @@
+﻿using Task.Backend;
+
+var t1 = new Time();
+var t2 = new Time(14);
+var t3 = new Time(9, 34);
+var t4 = new Time(19, 45, 56);
+var t5 = new Time(23, 3, 45, 678);
+
+var times = new List<Time> { t1, t2, t3, t4, t5};
+
+foreach (Time time in times)
+{
+    Console.WriteLine($"Time: {time}");
+}
